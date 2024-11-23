f17a8aac80d1cd25374ad834218dcd716ee6fb08
AymanNagyAhmed <aymannagy.ahmed@gmail.com>
2024-11-24 03:21:31 +0400

    - Add mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { PostsModule } from '@/modules/posts/posts.module';
 import { UsersModule } from '@/modules/users/users.module';
-import { getTypeOrmConfig } from '@/config/typeorm.config';
+import { getMongoConfig } from '@/config/mongo.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
+    MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: getTypeOrmConfig,
       inject: [ConfigService],
+      useFactory: getMongoConfig,
     }),
     PostsModule,
     UsersModule,
